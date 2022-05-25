53a569b9401bb2ee484deed4c806ba8da87b205c
Michael Hüppe <mhueppe@uos.de>
2022-05-25 18:50:19 +0200

    Added assets

---
+++ GameDC/Assets/Scripts/TorpedoMovement.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class TorpedoMovement : MonoBehaviour
+{
+    [SerializeField] float speed = 0.5f;
+
+    [SerializeField] private GameObject submarine;
+    [SerializeField] private int  distance = 40;
+    
+    
+
+     private void Update()
+     {
+         
+         Movement();
+         if (Vector3.Distance(submarine.transform.position, transform.position) >distance)
+         {
+             Destroy(this.gameObject);
+         }
+    }
+
+    private void Movement()
+    {
+        Debug.Log("Hallo ich bin jules torpedo");
+        transform.Translate(-transform.forward * speed);
+    }
+
+    
+    
+
+
+
+ 
+}

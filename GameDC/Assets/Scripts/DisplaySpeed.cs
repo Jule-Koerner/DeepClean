97a6472b671379bcadca943ffacb0c9ac61fb44e
Jule Körner <jule-koerner99@web.de>
2022-05-26 17:39:34 +0200

    Hoffen wa mal es klappt und loif hömmal

--- GameDC/Assets/Scripts/DisplaySpeed.cs
+++ GameDC/Assets/Scripts/DisplaySpeed.cs
@@ -9,28 +9,35 @@ public class DisplaySpeed : MonoBehaviour
 
 
     [SerializeField] private List<Image> speeds = new List<Image>();
-    
+    [SerializeField] Color[] colors;
     [SerializeField] private int speedRemaining;
-    [SerializeField] private Color initColor = Color.gray;
-    [SerializeField] private Color color = Color.green;
+    private Color initColor = new Color(255/255,0,0,1);
+   // private Color newcolor = new Color(95, 158, 160);
 
     private int index = 0;
 
-    private void Start()
-    {
-        for (int i = 0; i < speeds.Count; i++)
-        {
-            speeds[i].color = initColor;
-        }
+     private void Start()
+     {
+         for (int i = 0; i < speeds.Count; i++)
+         {
+             speeds[i].GetComponent<Image>().color = Color.black;
+             //speeds[i].color = initColor;
+    
+         }
         
     }
     // Update is called once per frame
     public void increaseSpeed()
+    
     {
+       
         if (index != speeds.Count)
         {
+            Color col = colors[index];
+            col.a = 1;
             //Decrease value of speedRemaining
-            speeds[index].color = color;
+            //speeds[index].color = Color.white;
+            speeds[index].GetComponent<Image>().color = col;
             index++;
             // Color one dot
             
@@ -41,8 +48,12 @@ public class DisplaySpeed : MonoBehaviour
     {
         if (index >= 0)
         {
-            speeds[index - 1].color = initColor;
-            index--;
+            speeds[index - 1].color = Color.black;
+            if (index != 0)
+            {
+                index--;
+            }
+          
         }
     }
 
--- GameDC/Assets/Scripts/PlayerMovement.cs
+++ GameDC/Assets/Scripts/PlayerMovement.cs
@@ -11,18 +11,17 @@ public class PlayerMovement : MonoBehaviour
 
     [SerializeField] private GameObject ui;
 
-    [SerializeField] public float speed = 0.05f;
-    private float speedChange = 0.005f;
+    [SerializeField] public float speed = 5f;
+    private float speedChange = 1f;
     
-    private float maxSpeed = 0.3f;
-    private float minSpeed = 0f;
+    private float maxSpeed = 10f;
+    private float minSpeed = 5f;
 
     private float rotationSpeed = 5;
 
     
     private float rot = 0f;
     private float rotY = 0f;
-
     private Rigidbody rb;
     
     //[SerializeField] private Transform startRay;
@@ -39,7 +38,7 @@ public class PlayerMovement : MonoBehaviour
     {
         propellerRotation = propeller.transform.rotation; 
         rb = GetComponent<Rigidbody>();
-        speedChange = speed / 2;
+        //speedChange = speed;
 
     }
 
@@ -49,14 +48,23 @@ public class PlayerMovement : MonoBehaviour
         // propeller.transform.rotation = Quaternion.Euler(propellerRotation.x + speed, propellerRotation.y, propellerRotation.z);
         // propellerRotation = propeller.transform.rotation; 
         propeller.transform.Rotate(0, speed*2, 0);
-        Speed();
+       
         Movement();
         Shoot();
-        //Speed
+        Speed();
+        Debug.Log("SPEEED"+ speed);
 
 
 
     }
+
+    private void FixedUpdate()
+    {
+        //Speed();
+        //rb.MovePosition(transform.position + transform.TransformDirection(transform.forward) * Time.deltaTime * speed);
+        transform.position += transform.forward * speed * Time.deltaTime;
+    }
+
 
     void Speed()
     {
@@ -64,7 +72,7 @@ public class PlayerMovement : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.RightArrow))
         {
             speed += speedChange;
-            //ui.GetComponent<DisplaySpeed>().increaseSpeed();
+            ui.GetComponent<DisplaySpeed>().increaseSpeed();
             
         }
         
@@ -72,9 +80,9 @@ public class PlayerMovement : MonoBehaviour
         else if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
             speed -= speedChange;
-            //ui.GetComponent<DisplaySpeed>().decreaseSpeed();
+            ui.GetComponent<DisplaySpeed>().decreaseSpeed();
         }
-        // speed = Mathf.Clamp(speed, -maxSpeed, maxSpeed);
+        speed = Mathf.Clamp(speed, 5, maxSpeed);
 
     }
 
@@ -84,7 +92,8 @@ public class PlayerMovement : MonoBehaviour
         //transform.Translate(transform.forward * 0.0005f);
         //rb.AddRelativeForce(-transform.forward * 0.05f);
         // Move position ist bisschen wie motor, moved genau da hin wo ich will (full direct contro, not affected by physics itself but works for affecting others)
-        rb.MovePosition(transform.position + transform.TransformDirection(transform.forward) * speed);
+        //rb.MovePosition(transform.position + transform.TransformDirection(transform.forward) * speed * Time.deltaTime);
+       // transform.Translate(transform.forward * speed * Time.deltaTime);
         //Rotation
          mouseAxisX = Input.GetAxis("Mouse X");
          rot += mouseAxisX * rotationSpeed/2;
@@ -95,18 +104,23 @@ public class PlayerMovement : MonoBehaviour
          //Rotation
          mouseAxisY = Input.GetAxis("Mouse Y");
          rotY += mouseAxisY * rotationSpeed/2;
+         
+        // Debug.Log("angle" + rotY);
          //rot = Mathf.Clamp(rot, -100f, 100f);
          //transform.eulerAngles = new (90f, rot, 0f);
-        
-         rb.rotation = Quaternion.Euler(-rotY, rot,  0f);
-         if ((transform.forward.z < 0 && speed > 0) || transform.forward.z > 0 && speed < 0)
-         {
-             speed *= -1;
-             // transform.forward = new Vector3(transform.forward.x * -1, transform.forward.y * -1, transform.forward.z);
-             // rb.rotation = Quaternion.Euler(rotY, rot,  0f);
-             
-         }
-         // Debug.Log(transform.forward +" speed: "+ speed);
+         
+         rot = Mathf.Clamp(rot, -90, 90);
+         //rotY = Mathf.Clamp(rot, -45, 90);
+
+         rb.MoveRotation(Quaternion.Euler(-rotY, rot, 0f));
+         // if ((transform.forward.z < 0 && speed > 0) || transform.forward.z > 0 && speed < 0)
+         // {
+         //     speed *= -1;
+         //     // transform.forward = new Vector3(transform.forward.x * -1, transform.forward.y * -1, transform.forward.z);
+         //     // rb.rotation = Quaternion.Euler(rotY, rot,  0f);
+         //     
+         // }
+         // // Debug.Log(transform.forward +" speed: "+ speed);
 
          // Debug.Log(rb.rotation);
         
@@ -129,6 +143,7 @@ public class PlayerMovement : MonoBehaviour
     {
         if (Input.GetKeyDown("space"))
         {
+            Debug.Log("SHOOOOt");
             Instantiate(torpedo, transform.GetChild(0).position, transform.rotation);
             
 
